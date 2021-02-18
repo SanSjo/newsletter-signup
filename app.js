d64d54f9f0c0e00e57af1abf0d24de5521c05164
susanna <susanna.sjoberg@fredenheim.se>
2021-02-18 23:36:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req,res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const result = firstName + lastName + email;
+    console.log(firstName, lastName, email);
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                   
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+    const url = "https://us1.api.mailchimp.com/3.0/lists/d5ec89d04f";
+    const options = {
+        method: "POST",
+        auth: "sus1:afb14ddd5f058cf92fb1033665a29c682-us1"
+    }
+
+
+
+        const request = https.request(url, options, function(response){
+
+            if(response.statusCode === 200) {
+                
+                res.sendFile(__dirname + "/success.html");
+            } else 
+            {
+                res.sendFile(__dirname + "/failure.html");
+
+            }
+
+            response.on("data", function(data) {
+                console.log(JSON.parse(data));
+            })
+        });
+    
+        request.write(jsonData);
+        request.end();
+
+  
+   
+
+    
+
+
+   
+})
+
+app.post("/failure", function(req, res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() { //works both on heroku and locally
+    console.log("Server is running on 3000")
+})
+
+//Api key
+//fb14ddd5f058cf92fb1033665a29c682-us1
+
+// List id
+// d5ec89d04f
